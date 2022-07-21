d7e49210b3a6b506018be8500cda801925d236c2
Artem B <artem.bv@pm.me>
2022-07-21 11:46:52 +0300

    feat: update deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,14 +5,11 @@ async function main() {
   const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
   const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const MFT = await ethers.getContractFactory("MFT");
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  const mft = await MFT.deploy();
+  await mft.deployed();
+  console.log("MFT contract deployed at:", mft.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
