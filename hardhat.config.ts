6747037febecbea4bfedbe61fd2dc1775985dde6
Artem B <artem.bv@pm.me>
2022-07-21 10:09:43 +0300

    feat: add networks to the config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,19 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "dotenv";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const { PRIVATE_KEY, API_URL } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   defaultNetwork: "rinkeby",
-  networks: [
+  networks: {
     hardhat: {},
     rinkeby: {
       url: API_URL,
       accounts: [ `0x${PRIVATE_KEY}` ]
     }
-  ]
+  }
 };
 
 export default config;
